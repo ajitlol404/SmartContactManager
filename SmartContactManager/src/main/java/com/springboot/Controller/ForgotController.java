359ecf1f802df9df8015813131a96128e64836ca
Ajit Kumar Neeraj <you@example.com>
2021-05-04 20:05:32 +0530

    Added forgot password module

---
+++ SmartContactManager/src/main/java/com/springboot/Controller/ForgotController.java
@@ -0,0 +1,117 @@
+package com.springboot.Controller;
+
+import java.util.Random;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.springboot.Entity.User;
+import com.springboot.Repository.UserRepository;
+import com.springboot.Service.EmailService;
+
+@Controller
+public class ForgotController 
+{
+	
+	@Autowired
+	private EmailService emailService;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	Random random = new Random();
+	
+	@RequestMapping("/forgot")
+	public String openEmailForm()
+	{
+		return "forgot_email_form";
+		
+	}
+	
+	@PostMapping("/send-otp")
+	public String sendOTP(@RequestParam("registeredemail") String email,HttpSession session)
+	{
+		System.out.println("Registered email : "+ email);
+		
+		//generate otp of 4 digit
+		int otp = random.nextInt(9999);
+		System.out.println("Otp: "+otp);
+		
+		String subject="OTP from Smart Contact Manager.";
+		String message=""
+				+ "<div style='border:1px solid #e2e2e2;padding:20px' >"
+				+ "<h2>"
+				+ "Otp is "
+				+ "<b>"+otp
+				+ "</n>"
+				+ "</h2>"
+				+ "</div>";
+		String to=email;
+		//write code for send otp
+		boolean result = emailService.sendEmail(subject, message, to);
+		
+		if(result)
+		{
+			session.setAttribute("myotp", otp);
+			session.setAttribute("email", email);
+			return "verifyOtp";
+		}
+		else
+		{
+			session.setAttribute("message", "Check your email id.");
+			return "forgot_email_form";
+		}
+		
+	}
+	
+	@PostMapping("/verify-otp")
+	public String verifyOtp(@RequestParam("otp")int otp,HttpSession session)
+	{
+		int myOtp=(int) session.getAttribute("myotp");
+		String email=(String) session.getAttribute("email");
+		
+		if(myOtp==otp)
+		{
+			//password change form
+			User user = userRepository.getUserByUserName(email);
+			if(user==null)
+			{
+				//send error message
+				session.setAttribute("message", "User doesnot exist with this email");
+				return "forgot_email_form";
+			}
+			else {
+				//send change password form
+			}
+			return "password_change_form";
+		}
+		else
+		{
+			session.setAttribute("message", "You have entered wrong OTP");
+			return "verifyOtp";
+		}
+		
+	}
+	
+	//change password
+	@PostMapping("/change-password")
+	public String changePassword(@RequestParam("newpassword")String newpassword,HttpSession session)
+	{
+		String email=(String) session.getAttribute("email");
+		User user = userRepository.getUserByUserName(email);
+		user.setPassword(bCryptPasswordEncoder.encode(newpassword));
+		
+		userRepository.save(user);
+		
+		return "redirect:/signin?change=password change successfullly.";
+	}
+}
---
+++ SmartContactManager/src/main/java/com/springboot/Service/EmailService.java
@@ -0,0 +1,76 @@
+package com.springboot.Service;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+	public boolean sendEmail(String subject,String message,String to)
+	{
+		boolean f=false;
+		
+		String from="tarunmicromax12@gmail.com";
+		// variable form gmail
+				String host = "smtp.gmail.com";
+
+				// get the system property
+				Properties properties = System.getProperties();
+				System.out.println(properties);
+
+				// setting important information to properties object
+				// host set
+				properties.put("mail.smtp.host", host);
+				properties.put("mail.smtp.port", "465");
+				properties.put("mail.smtp.ssl.enable", "true");
+				properties.put("mail.smtp.auth", "true");
+
+				// Step 1: to get the session object
+				Session session = Session.getInstance(properties, new Authenticator() {
+					@Override
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication("tarunmicromax12@gmail.com", "Ajit#8652");
+					}
+				});
+
+				session.setDebug(true);
+
+				// Step 2: compose the message
+				MimeMessage m = new MimeMessage(session);
+
+				try {
+
+					// from email
+					m.setFrom(from);
+
+					// adding recepient
+					m.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+					// adding subject to message
+					m.setSubject(subject);
+
+					// adding text to message
+					//m.setText(message);
+					m.setContent(message, "text/html");
+
+					// Step 3: send the message using transport class
+					Transport.send(m);
+
+					System.out.println("Message has been sent.");
+					f=true;
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				return f;
+			} 
+}
